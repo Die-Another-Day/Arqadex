5a35fbd1a59abd5a4724138e63551b29ff27d7b2
DADieAnotherDay <eeshan.agrawal.779@gmail.com>
2026-05-27 15:07:37 +0530

    New feature

--- components/GameGrid.tsx
+++ components/GameGrid.tsx
@@ -8,7 +8,7 @@ const UPCOMING_GAMES = [
 {
   id: 'merge-ball',
   title: 'MERGE BALL',
-  url: '/merge-ball',
+  url: 'https://merge-colour.vercel.app',
   description:
     'Physics-driven chaotic merge experience with neon arcade aesthetics. Drop, collide, and evolve unstable quantum spheres.',
   tags: ['PHYSICS', 'CASUAL', 'NEON'],
