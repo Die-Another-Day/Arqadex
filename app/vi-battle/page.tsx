4327104caf432e6a7a250cd3f5461a6c2db5cda0
DADieAnotherDay <eeshan.agrawal.779@gmail.com>
2026-05-28 23:49:10 +0530

    vi-battle

---
+++ app/vi-battle/page.tsx
@@ -0,0 +1,10 @@
+export default function VIBattlePage() {
+  return (
+    <main className="w-full h-screen overflow-hidden bg-black">
+      <iframe
+        src="/vi-battle/index.html"
+        className="w-full h-full border-0"
+      />
+    </main>
+  )
+}
