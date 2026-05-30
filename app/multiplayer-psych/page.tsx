f0a8443de8a3b641b6af657599cf55e98715c689
DADieAnotherDay <eeshan.agrawal.779@gmail.com>
2026-05-30 16:35:04 +0530

    Final

--- app/multiplayer-psych/page.tsx
+++ app/multiplayer-psych/page.tsx
@@ -2,7 +2,7 @@ export default function MultiplayerPsych() {
   return (
     <main className="w-full h-screen overflow-hidden bg-black">
       <iframe
-        src="/multiplayer-psych/index.html"
+        src="/multiplayer-psych-static/index.html"
         className="w-full h-full border-0"
       />
     </main>
--- app/signal-collapse/page.tsx
+++ app/signal-collapse/page.tsx
@@ -2,7 +2,7 @@ export default function SignalCollapsePage() {
   return (
     <main className="w-full h-screen overflow-hidden bg-black">
       <iframe
-        src="/signal-collapse/index.html"
+        src="/signal-collapse-static/index.html"
         className="w-full h-full border-0"
       />
     </main>
